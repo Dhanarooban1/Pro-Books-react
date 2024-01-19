944844999cfbbf577763b5cabafaeff5a4ccf925
santhosh-005 <santhoshdon000555@gmail.com>
2024-01-19 10:00:10 +0530

    finished

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import './App.css'
+
+function App() {
+
+  const [books,setBooks]=useState([])
+
+  console.log(books)
+
+  useEffect(()=>{
+    axios.get("https://reactnd-books-api.udacity.com/books",{ headers: { 'Authorization': 'whatever-you-want' }})
+    .then((res)=>setBooks(res.data.books))
+    .catch((err)=>console.error("Error Found",err))
+  },[])
+
+  return (
+    <div>
+      <h1>Pro Books</h1>
+      <div className="container">
+      {
+        books.length==0 && (
+          <div className='loader'>
+
+          </div>
+        )
+      }
+      {
+        books.map((book,index)=>{
+          return (
+            <div className='book' key={index}>
+              <h2>{book.title}</h2>
+              <div>
+                <img src={book.imageLinks.smallThumbnail} alt="Thumbnail" />
+                <p>{book.description}</p>
+              </div>
+            </div>
+          )
+        })
+      }
+      </div>
+    </div>
+  )
+}
+
+export default App
